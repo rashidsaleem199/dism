ac2b8ebc55c3ba79b41130328e073f105c2caac9
rashidsaleem199 <abdulbasit0343@gmail.com>
2024-06-03 11:27:45 +0500

    add calculator work

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+let oprands = (number)=>{
+    document.querySelector("#inp").value +=number
+}
+let operator = (op)=>{
+    document.querySelector("#inp").value +=op
+
+}
+let calculator = ()=>{
+    let result = eval(document.querySelector("#inp").value);
+document.querySelector("#inp").value = result;
+}
+let clearall=()=>{
+    document.querySelector("#inp").value ="";
+}
